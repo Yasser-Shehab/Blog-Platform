9fe4ecbe1570254c27efdd5cc3d8c5ff77b3ee3a
Yasser-Shehab-El-Dean <yasser@axlr8.tech>
2025-02-20 16:08:29 +0200

    Adjusted The Gap between the Header And the posts

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,7 +40,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           <main className="min-h-screen flex flex-col items-center">
-            <div className="flex-1 w-full flex flex-col gap-20 items-center">
+            <div className="flex-1 w-full flex flex-col gap-10 items-center">
               <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
                 <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
                   <div className="flex gap-5 items-center font-semibold">
@@ -51,7 +51,7 @@ export default function RootLayout({
                 </div>
               </nav>
               <ToastProvider />
-              <div className="flex flex-col gap-20 max-w-5xl p-5">
+              <div className="flex flex-col gap-10 max-w-5xl p-5">
                 {" "}
                 <UserProvider>{children}</UserProvider>
               </div>
