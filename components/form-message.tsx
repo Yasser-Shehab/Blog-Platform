04201cacd6690e4a868fb08c6504b4b667b5d4b4
Yasser-Shehab-El-Dean <yasser@axlr8.tech>
2025-02-20 15:50:52 +0200

    Adjusted the Error Message color in Dark / Light mode

--- components/form-message.tsx
+++ components/form-message.tsx
@@ -1,23 +1,22 @@
-export type Message =
-  | { success: string }
-  | { error: string }
-  | { message: string };
+export type Message = { success: string } | { error: string } | { message: string };
 
 export function FormMessage({ message }: { message: Message }) {
   return (
     <div className="flex flex-col gap-2 w-full max-w-md text-sm">
       {"success" in message && (
-        <div className="text-foreground border-l-2 border-foreground px-4">
+        <div className="text-green-600 dark:text-green-400 border-l-2 border-green-600 dark:border-green-400 px-4">
           {message.success}
         </div>
       )}
       {"error" in message && (
-        <div className="text-destructive-foreground border-l-2 border-destructive-foreground px-4">
+        <div className="text-red-600 dark:text-red-400 border-l-2 border-red-600 dark:border-red-400 px-4">
           {message.error}
         </div>
       )}
       {"message" in message && (
-        <div className="text-foreground border-l-2 px-4">{message.message}</div>
+        <div className="text-gray-700 dark:text-gray-300 border-l-2 border-gray-700 dark:border-gray-300 px-4">
+          {message.message}
+        </div>
       )}
     </div>
   );
