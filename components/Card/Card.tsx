49997705036a543577fe15d22646146f61f70d2e
Yasser-Shehab-El-Dean <yasser@axlr8.tech>
2025-02-20 15:56:04 +0200

    Adjusted the text alignment

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -17,8 +17,8 @@ type CardProps = {
 const Card = ({ post, user }: CardProps) => {
   return (
     <div className="border p-4 rounded-lg shadow-md max-w-md ">
-      <h2 className="text-2xl font-bold mb-5">{post.title}</h2>
-      <p className="text-lg md:text-xl text-gray-800 dark:text-gray-200 leading-relaxed text-justify md:text-center mb-4 break-words overflow-hidden max-w-full whitespace-pre-wrap">
+      <h2 className="text-2xl font-bold mb-5 text-center">{post.title}</h2>
+      <p className="text-lg md:text-xl text-gray-800 text-left  dark:text-gray-200 leading-relaxed  md:text-left   mb-4 break-words overflow-hidden max-w-full whitespace-pre-wrap">
         {post.content}
       </p>
 
